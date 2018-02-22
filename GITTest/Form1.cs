939de206d311dd111d4d63c7893f06e288875954
p10364658 <p10364658@GWHXX280WIN014W>
2018-02-22 12:20:13 +0000

    Followed the lab session to the reader code

--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,27 @@ namespace GITTest
         private void label2_Click(object sender, EventArgs e)
         {
             //comment here
+        }
+
+        private void btnGetDates_Click(object sender, EventArgs e)
+        {
+            List<string> Dates = new List<string>();
+            //clear the listbox
+            listBoxDates.Items.Clear();
+            //create the database string
+            string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date] from Sheet1", connection);
+                reader = getDates.ExecuteReader();
+                while (reader.Read())
+                {
+                    Dates.Add(reader[0].ToString());
+                    Dates.Add(reader[1].ToString());
+                }
+            }
         }
     }
 }
