0f70c2ce15cd62c10df0b42b594a13cdd0b96971
p16244305 <p16244305@GWHXX280WIN013W>
2018-02-22 12:04:36 +0000

    Add code in form1.cs

--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,29 @@ namespace GITTest
         private void label2_Click(object sender, EventArgs e)
         {
             //comment here
+        }
+
+        private void btnGetDates_Click(object sender, EventArgs e)
+        {
+            //Create new list to store the results in
+            List<string> Dates = new List<string>();
+            //clear the listbox
+            lstBoxDates.Items.Clear();
+
+            //Create the database string
+            string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+
+            using(OleDbConnection connection=new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date],[Ship Date] from Sheet1", connection);
+
+                reader = getDates.ExecuteReader();
+                //aaab
+            }
+
+            
         }
     }
 }
