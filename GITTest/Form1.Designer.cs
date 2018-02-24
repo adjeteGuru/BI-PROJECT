116391570db7cd06a9080c4ed7877cf8810f04f0
bzi1030765617 <bzi1030765617@gmail.com>
2018-02-24 11:08:24 +0000

    Add control to the Form1

--- GITTest/Form1.Designer.cs
+++ GITTest/Form1.Designer.cs
@@ -32,14 +32,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnGetDates = new System.Windows.Forms.Button();
             this.lstBoxDates = new System.Windows.Forms.ListBox();
+            this.btnGetCustomer = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(13, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.Size = new System.Drawing.Size(149, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "Git clone and pull test!";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -47,18 +49,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 77);
+            this.label2.Location = new System.Drawing.Point(51, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.Size = new System.Drawing.Size(11, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = ".";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // btnGetDates
             // 
-            this.btnGetDates.Location = new System.Drawing.Point(16, 51);
+            this.btnGetDates.Location = new System.Drawing.Point(16, 47);
             this.btnGetDates.Name = "btnGetDates";
-            this.btnGetDates.Size = new System.Drawing.Size(75, 23);
+            this.btnGetDates.Size = new System.Drawing.Size(75, 21);
             this.btnGetDates.TabIndex = 2;
             this.btnGetDates.Text = "GetDates";
             this.btnGetDates.UseVisualStyleBackColor = true;
@@ -67,16 +69,37 @@
             // lstBoxDates
             // 
             this.lstBoxDates.FormattingEnabled = true;
-            this.lstBoxDates.Location = new System.Drawing.Point(16, 133);
+            this.lstBoxDates.ItemHeight = 12;
+            this.lstBoxDates.Location = new System.Drawing.Point(16, 123);
             this.lstBoxDates.Name = "lstBoxDates";
-            this.lstBoxDates.Size = new System.Drawing.Size(120, 95);
+            this.lstBoxDates.Size = new System.Drawing.Size(120, 88);
             this.lstBoxDates.TabIndex = 3;
+            // 
+            // btnGetCustomer
+            // 
+            this.btnGetCustomer.Location = new System.Drawing.Point(166, 47);
+            this.btnGetCustomer.Name = "btnGetCustomer";
+            this.btnGetCustomer.Size = new System.Drawing.Size(82, 23);
+            this.btnGetCustomer.TabIndex = 4;
+            this.btnGetCustomer.Text = "GetCustomer";
+            this.btnGetCustomer.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(166, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(426, 241);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGetCustomer);
             this.Controls.Add(this.lstBoxDates);
             this.Controls.Add(this.btnGetDates);
             this.Controls.Add(this.label2);
@@ -94,6 +117,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnGetDates;
         private System.Windows.Forms.ListBox lstBoxDates;
+        private System.Windows.Forms.Button btnGetCustomer;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
