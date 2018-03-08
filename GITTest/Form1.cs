6931fdbd242e5ddfbd919e56aac939d92adde8e1
ighoakponah <ighoakponah@IGHOAKPONAH7872>
2018-03-08 16:18:15 +0000

    added the concatenation comment

--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -97,7 +97,7 @@ namespace GITTest
                 {
 
                     //we enlist the columns to be read 
-                    
+                    //we use concatenation to read all rows in a single line
                     Products.Add(reader[0].ToString() + ", " + reader[1].ToString() + ", " + reader[2].ToString() + ", " + reader[3].ToString() + ", " + reader[4].ToString() + ", " + reader[5].ToString());
 
 
